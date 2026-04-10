b6c26916ba382ae2245d2ba91cc8a5b419e2e57c
Raj Prasad <rajp182138@gmail.com>
2026-04-11 02:06:44 +0530

    make some changes

--- Backend/services/ai.js
+++ Backend/services/ai.js
@@ -1,29 +1,3 @@
-// import OpenAI from 'openai';
-
-// const openai = new OpenAI({
-//   apiKey: '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',
-//   baseURL: 'https://integrate.api.nvidia.com/v1',
-// })
- 
-// async function main() {
-//   const completion = await openai.chat.completions.create({
-//     model: "qwen/qwen3-coder-480b-a35b-instruct",
-//     messages: [{"role":"user","content":""}],
-//     temperature: 0.7,
-//     top_p: 0.8,
-//     max_tokens: 4096,
-//     stream: true
-//   })
-   
-//   for await (const chunk of completion) {
-//     process.stdout.write(chunk.choices[0]?.delta?.content || '')
-//   }
-  
-// }
-
-// main();
-
-
 import OpenAI from "openai";
 import dotenv from "dotenv";
 
